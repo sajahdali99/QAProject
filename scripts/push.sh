8deb0edf1e14ba1a739f5845c3b6f7017b39fb94
sajahdali99 <71330997+sajahdali99@users.noreply.github.com>
2020-11-06 12:28:19 +0000

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,15 +1,3 @@
 #! /bin/bash
 
-sudo docker login -u admin -p admin localhost:8082
-
-sudo docker tag service-1:latest localhost:8082/service-1:latest
-sudo docker push localhost:8082/service-1
-
-sudo docker tag service-2:latest localhost:8082/service-2:latest
-sudo docker push localhost:8082/service-2
-
-sudo docker tag service-3:latest localhost:8082/service-3:latest
-sudo docker push localhost:8082/service-3
-
-sudo docker tag service-4:latest localhost:8082/service-4:latest
-sudo docker push localhost:8082/service-4
+sudo docker-compose push
